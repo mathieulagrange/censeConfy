0a383b519a6634b56373d20972d93f03dd6e888d
mathieu <>
2020-11-27 09:38:57 +0100

    new preparedata

---
+++ censeConfy.py
@@ -0,0 +1,69 @@
+import explanes as el
+from pandas import DataFrame
+import time
+import numpy as np
+from pathlib import Path
+import os
+
+if __name__ == "__main__":
+  el.experiment.run()
+
+# use case where:
+#   - the results are stored on disk using npy files
+#   - one factor affects the size of the results vectors
+#   - the metrics does not operate on the same data, resulting on result vectors with different sizes per metric
+
+def set(args):
+  experiment = el.experiment.Experiment()
+  experiment.project.name = 'censeConfy'
+  experiment.project.description = 'confy study using the cense sensor network'
+  experiment.project.author = 'mathieu Lagrange'
+  experiment.project.address = 'mathieu.lagrange@ls2n.fr'
+  experiment.project.version = '0.1'
+
+  experiment.path.input = '~/data/storage/cense/confy/data/'
+  experiment.path.predict = '~/data/experiments/'+experiment.project.name+'/predict/'
+  experiment.path.aggregate = '~/data/experiments/'+experiment.project.name+'/aggregate/'
+  experiment.path.code = '~/experiments/censeConfy/'
+  # experiment.path.tmp = '/tmp/'+experiment.runId
+  experiment.setPath()
+
+  experiment._archivePath = '/tmp'
+
+  experiment.host = ['pc-lagrange.irccyn.ec-nantes.fr']
+
+  experiment.factor.task = ['predict', 'aggregate']
+  experiment.factor.month = ['january', 'march']
+  experiment.factor.reduce = ['none', 'OneDay', 'OneDayOneHour']
+  experiment.factor.sensor = list(range(16))
+  experiment.factor.period = ['none', 'day', 'evening', 'night']
+
+  experiment.metric.t = ['mean', 'std']
+  experiment.metric.v = ['mean', 'std']
+  experiment.metric.b = ['mean', 'std']
+  experiment.metric.duration = ['mean']
+  return experiment
+
+def step(setting, experiment):
+  tic = time.time()
+  if setting.task is 'predict':
+    dataFileName = 'censeConfy'+setting.month
+    if setting.reduce is not 'none':
+      dataFileName += setting.reduce
+    dataFileName = experiment.path.input+dataFileName+'/'+dataFileName+'_sensor_'+str(setting.sensor)+'_spec.npy'
+    print(dataFileName)
+    command = 'cd ../censeDomainSpecialization && python3 inference.py --dataset TVBCense_dev -rnn --datasetName '+dataFileName+' --outputPath  '+experiment.path.predict+' --pretrained vec -finetune'+' --outputName  '+setting.id()
+    print(command)
+    os.system(command)
+
+  baseFileName = setting.id()
+  duration = time.time()-tic
+  np.save(os.path.expanduser(experiment.path.predict+baseFileName+'_duration.npy'), duration)
+
+# uncomment this method to fine tune display of metrics
+def display(experiment, settings):
+  (data, desc, header)  = experiment.metric.get('mae', settings, experiment.path.output, settingEncoding = experiment._settingEncoding)
+
+  print(header)
+  print(desc)
+  print(len(data))
---
+++ prepareDataNpy.py
@@ -0,0 +1,118 @@
+import numpy as np
+import tables as tb
+import os
+import glob
+import csv
+import shutil
+import tqdm as tq
+import argparse
+import matplotlib.pyplot as plt
+plt.rcParams["figure.figsize"] = [16,9]
+
+from utils import *
+
+class Config:
+    pass
+
+class Sensor(tb.IsDescription):
+  number = tb.Int32Col()
+  id = tb.StringCol(100)
+  lat = tb.Float64Col()
+  lon = tb.Float64Col()
+
+config = Config()
+config.nbFrequencyBands = 29
+config.nbVec = 351
+
+config.rootPath = '/home/lagrange/data/storage/cense/confy/'
+config.inputPath = config.rootPath+'raw/'
+config.dataPath = config.rootPath+'data/'
+
+config.sensorInfo = sensor_list('sensorList.csv')
+
+config.month = ['january', 'march']
+config.monthPath = ['01_02_2020', '04_05_2020']
+
+config.localPath = '/home/lagrange/drive/experiments/data/local/censeConfy/'
+
+def generateDataset(config):
+  tmpFileName = config.dataPath+'tmpDa'
+  dataId = 'censeConfy'+config.month[config.monthType]
+
+  if config.oneDay:
+    dayLimit = 2
+  else:
+    dayLimit = 33
+  if config.oneHour:
+    hourLimit = 2
+  else:
+    hourLimit = 25
+
+
+  if config.oneDay:
+    dataId += 'OneDay'
+  if config.oneHour:
+    dataId += 'OneHour'
+  print(dataId)
+  if not os.path.exists(config.dataPath+dataId):
+      os.makedirs(config.dataPath+dataId)
+  for sCount, s in enumerate(config.sensorInfo): #tq.tqdm(enumerate(sensorInfo), total=len(sensorInfo)):
+    print('Sensor '+str(sCount)+' / '+str(len(config.sensorInfo)))
+
+    if dryRun:
+      data = np.zeros((10, 32))
+      arrayTime.append(data[:, 0])
+      arraySpec.append(data[:, 3:])
+    else:
+      fileNames = []
+      for year in [2019, 2020]:
+        for month in range(13):
+          for day in range(dayLimit):
+            for hour in range(hourLimit):
+              f = config.inputPath+config.monthPath[mType]+'/'+s["sID"]+'/'+str(year)+'/'+str(month)+'/'+str(day)+'/'+str(hour)+'.zip'
+              if os.path.exists(f):
+                fileNames.append(f)
+      fCount = 0
+      arrayTime = np.zeros((len(fileNames)*6))
+      arraySpec = np.zeros((len(fileNames)*6, config.nbVec, config.nbFrequencyBands))
+
+      for inputFileName in tq.tqdm(fileNames, total=len(fileNames)):
+        shutil.copy(inputFileName, tmpFileName+'.zip')
+        os.system('unzip -d /tmp '+tmpFileName+'.zip')
+        csvFileName = os.path.basename(inputFileName).replace('zip', 'csv')
+        with open('/tmp/'+csvFileName, 'r') as csvfileID:
+          reader = csv.reader(csvfileID, delimiter=',')
+
+          data = np.zeros((config.nbVec, 32))
+          rCount = 0
+          for r, row in enumerate(reader):
+            if r%4800<config.nbVec: # every 10 minutes, for 45 seconds
+                #print(rCount)
+              data[rCount, :] = np.array([float(s) for s in row])
+              rCount +=1
+            elif r%4800==config.nbVec:
+              rCount = 0
+              arrayTime[fCount] = data[0, 0]
+              arraySpec[fCount, :, :] = data[:, 3:][None]
+              fCount += 1
+                #print(arraySpec.shape)
+          os.remove('/tmp/'+csvFileName)
+        os.remove(tmpFileName+'.zip')
+    np.save(config.dataPath+dataId+'/'+dataId+'_sensor_'+str(sCount)+'_time.npy', arrayTime)
+    np.save(config.dataPath+dataId+'/'+dataId+'_sensor_'+str(sCount)+'_spec.npy', arraySpec)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    # Model parameters
+    parser.add_argument('-D', action='store_true', help='OneDay')
+    parser.add_argument('-H', action='store_true', help='OneHour')
+    parser.add_argument('-C', action='store_true', help='confy')
+
+    args = parser.parse_args()
+    print(args)
+    config.oneDay = args.D
+    config.oneHour = args.H
+    config.monthType = args.C
+
+    generateDataset(config)
